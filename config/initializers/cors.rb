9e9b47343449658b860abdae000753746321b5e3
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2019-01-15 22:38:01 -0500

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://thuyanduong-flatiron.github.io'
 
     resource '*',
       headers: :any,
